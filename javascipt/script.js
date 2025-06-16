83a670a75d6d057f9c664e19518ac3474795284f
Ayush Singh <ayushsingh18777@gmail.com>
2025-06-16 19:40:31 +0530

    js 4 loops with array

---
+++ javascipt/script.js
@@ -0,0 +1,221 @@
+
+// let str = "Apna ColLEGE"
+// let index = 3;
+
+// if (str[index] == str[index].toLowerCase()) {
+//     console.log('lowercase');
+// }
+// else if (str[index] == str[index].toupperCase() ){
+//     console.log('uppercase');
+// }
+// else{
+//     console.log('not a letter');
+// }
+
+// let str = prompt('Enter a string');
+// console.log("original string: " + str);
+// console.log("after trim " + str.trim());
+
+// let arr = [1, 2, 3, 4, 5];
+// let item = 3;
+
+// if(arr.indexOf(item) !== -1){
+//     console.log('item exists ');
+// }
+// else{
+//     console.log('item does not exist');
+// }
+
+// for(let i = 1 ; i<= 15 ; i++){
+//    if (i % 2 != 0){
+//     console.log(i);
+//    }
+// }
+
+// for(let i = 2 ; i <=10; i +=2){
+//     console.log(i);
+// }
+
+//Multiplication table of 5 
+
+
+// let inp = prompt('Enter a number to print its multiplication table');
+// inp = parseInt(inp);
+// let num = inp;
+// for( num ; num <= inp*10 ; num+=inp){
+//     console.log(num);
+// }
+
+// let i = 0;
+// while (i<10){
+//     console.log(i);
+//     i++;
+// }
+// const favmovie = "avatar";
+
+// let guess = prompt("guess the movie name ");
+
+// while (( guess !== favmovie) && (guess != "quit")){
+//    console.log("wrong guess");
+//     guess = prompt("wrong guess , try again  ");
+// }
+
+// if (guess === favmovie) {
+// console.log("congratulations! you guessed it right");
+// }
+
+// else if (guess == "quit"){
+// console.log("you quit the game");
+// }
+
+// let fruits = ['mango', 'apple','banana','orange'];
+// for (let i=0; i<=fruits.length; i+2){
+//     console.log(i , fruits[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+// let msg = "   hello o    ";
+// msg = msg.trim().msg.toUpperCase();
+// console.log(msg);
+
+
+// let name = ['john ', 'doe', 'jane'];
+// console.log(name[0]);
+
+
+// let elements = [ 7 , 8 , 9 , 3 , 2];
+// let n = 5;
+
+// let ans = elements.slice(0, n);
+// let ans1 = elements.slice(0, n-1);
+// console.log(ans);
+// console.log(ans1);
+
+//  let str = prompt('Enter a string');
+//  if( str == ""){
+
+ 
+//     console.log('string is empty');}
+//     else{
+//         console.log('string is not empty');
+//     }
+ 
+// let color = 'red';
+// if(color === 'green'){
+//     console.log('go');
+// }
+//     else if(color === 'red'){
+//         console.log('stop in the name of law');
+//     }
+//   else if(color === 'yellow'){
+//         console.log('slow down');
+//     }
+
+//     else{
+//         console.log('invalid color');
+//     }
+
+// let size  = 'L';
+// if(size === 'XL'){
+//     console.log('small size');
+// }
+// else if(size === 'L'){
+//     console.log('Large size');
+// }
+// else if(size === 'M'){
+//     console.log('Medium size');
+// }
+// else if(size === 'S'){
+//     console.log('Small size');
+// }
+// else{
+//     console.log('invalid size');
+// }
+
+
+// let goodstring = 'apple'
+// if(goodstring.length >= 5 && goodstring[0]=='a'){
+//     console.log('good string');
+// }
+//     else{
+//         console.log('bad string');
+//     }
+
+
+//     prompt('Enter your name');
+// alert('hello world');
+
+
+// let num = 10;
+// if (num % 10 ==0){
+//     console.log('good');
+// }
+// else{
+//     console.log('bad');
+// }
+
+// let name = prompt('Enter your name');
+// let age = prompt('Enter your age');
+
+// alert('Hello ' + name + ' you are ' + age + ' years old');
+
+// let quarter = prompt('Enter the quarter number (1-4):');
+
+// switch (quarter) {
+//     case '1':
+//         console.log('January, February, March');
+//         break;
+//     case '2':
+//         console.log('April, May, June');
+//         break;
+//     case '3':
+//         console.log('July, August, September');
+//         break;
+//     case '4':
+//         console.log('October, November, December');
+//         break;
+//     default:
+//         console.log('Invalid quarter number. Please enter a number between 1 and 4.');
+// }
+
+// let goldstring = prompt('Enter a string');
+
+// if( goldstring.length >= 5 && (goldstring[0] == 'a' || goldstring[0] == 'A')){
+//     console.log('good string');
+// }
+// else{
+//     console.log('bad string');
+// }
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// if (a > b && a > c) {
+//     console.log('a is the largest number');
+// }
+// else if (b > a && b > c) {
+//     console.log('b is the largest number');
+// }
+// else {
+//     console.log('c is the largest number');
+// }
+
+// let num = 10;
+// let num2 = 20;
+
+// if((num % 10 == 0) && (num2 % 10 == 0)){
+//     console.log(' the last number is same that is' +  num % 10);
+// }
+
+// else{
+//     console.log('not same');
+// }
